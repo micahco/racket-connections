6d046f0f009088cdfde7cf9a4ee0d0403287d922
Micah Cowell <micahcowell@gmail.com>
2024-09-29 18:59:11 +0000

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+# Execute SQL queries
 for file in sql/*.sql; do
     if [ -f "$file" ]; then
         cat "$file" | psql -h localhost -d postgres -U postgres
     fi        
 done
+
+# Create local developer env
+cp .env.public .env
